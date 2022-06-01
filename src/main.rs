7c97a65344773f8e4cbe9f34cec08d13b994e7aa
calum <sc18csw@leeds.ac.uk>
2022-06-01 16:05:28 +0100

    structopt example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "clipd", about = "A slightly smart clipboard.")]
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    pub file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt.file);
 }
